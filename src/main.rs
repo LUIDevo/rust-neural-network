ccbec5821091b089aaa7485fbd616939b646415f
louis <sanicisspeedandfast@gmail.com>
2026-07-09 17:35:40 -0400

    getting rid of training loop

--- src/main.rs
+++ src/main.rs
@@ -59,39 +59,6 @@ fn main() {
         iterations: 0,
     };
     let mut out;
-    for iterations in 0..=1000 {
-        optimiser.pre_update();
-        // define forward pass
-        out = x.clone();
-        for layer in layers.iter_mut() {
-            out = layer.forward(&out);
-        }
-        let (loss, accuracy) = output.forward(&out, &target);
-        if iterations % 100 == 0 {
-            println!(
-                "Iteration: {}, Loss: {}, Accuracy: {}",
-                iterations, loss, accuracy
-            );
-        }
-        // define backward pass & update weights
-        let mut grad = output.backward(&target);
-        for layer in layers.iter_mut().rev() {
-            grad = layer.backward(&grad);
-        }
-        for layer in layers.iter_mut() {
-            if let Layer::Dense(dense) = layer {
-                optimiser.update_params(dense);
-            }
-        }
-    }
-    out = test_x;
-    for layer in layers.iter_mut() {
-        match layer {
-            Layer::Dropout(_) => (),
-            _ => out = layer.forward(&out),
-        }
-    }
-    let test_target = Target::Dense(test_y);
-    let (test_loss, test_acc) = output.forward(&out, &test_target);
-    println!("TEST  loss {:.3}  acc {:.3}", test_loss, test_acc);
+    // training loop
+    // test loop
 }
