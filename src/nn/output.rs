feb746ef9907b02d7e4f4f8cfea63c01576727a1
louis <sanicisspeedandfast@gmail.com>
2026-09-03 23:45:06 -0400

    fixed softmax accuracy

--- src/nn/output.rs
+++ src/nn/output.rs
@@ -53,21 +53,27 @@ fn softmax(inputs: &Matrix) -> Vec<f32> {
 impl SoftmaxLossCategoricalCrossEntropy {
     pub fn calculate_accuracy(&self, probabilities: &Matrix, y_true: &Vec<usize>) -> f32 {
         // find mean of percentage correct predictions
-        let samples = probabilities.data.len();
-        let correct = probabilities.data
-            .into_iter()
-            .zip(y_true)
-            .filter(|(p, y)| {
-                let pred = p
-                    .iter()
-                    .enumerate()
-                    .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
-                    .map(|(i, _)| i)
-                    .unwrap();
-                pred == **y
-            })
-            .count();
-        correct as f32 / samples as f32
+        let mut count:f32 = 0.0;
+        for (row, y) in probabilities.data.chunks(probabilities.cols()).zip(y_true) {
+            let pred = row.iter().copied().fold(f32::NEG_INFINITY, f32::max);
+            if pred==*y as f32 { count+=1.0; }
+        }
+        count/probabilities.data.len() as f32
+        // let samples = probabilities.data.len();
+        // let correct = probabilities.data
+        //     .into_iter()
+        //     .zip(y_true)
+        //     .filter(|(p, y)| {
+        //         let pred = p
+        //             .iter()
+        //             .enumerate()
+        //             .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+        //             .map(|(i, _)| i)
+        //             .unwrap();
+        //         pred == **y
+        //     })
+        //     .count();
+        // correct as f32 / samples as f32
     }
     pub fn calculate_loss(&self, probabilities: Matrix, y_true: &Vec<usize>) -> f32 {
         let exp_probabilities = probabilities
