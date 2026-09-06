7d9e7bb863ff6f4b28e3ac25474c2cdc9ce925ce
louis <sanicisspeedandfast@gmail.com>
2026-09-06 19:59:01 -0400

    added accuracy LinearMSE test

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,5 @@
 use crate::math::matrix::{dot, transpose, Matrix};
+use crate::nn::output::{LinearMeanSquaredError, Output, Target};
 
 #[test]
 fn dot_works() {
@@ -13,4 +14,15 @@ fn transpose_works() {
     let a = Matrix::new(vec![1.0, 2.0, 3.0, 4.0], 2, 2);
     let expected = Matrix::new(vec![1.0, 3.0, 2.0, 4.0], 2, 2);
     assert_eq!(transpose(&a), expected);
+}
+
+#[test]
+fn accuracy_works() {
+    let a = Matrix::new(vec![1.0, 2.0, 3.0, 4.0], 2, 2);
+    let b = Matrix::new(vec![5.0, 6.0, 7.0, 8.0], 2, 2);
+    let t = Target::Dense(b);
+    let mut output = Output::LinearMSE(LinearMeanSquaredError::default());
+    let result = output.forward(&a, &t);
+    println!("{}, {}", result.0, result.1); // test earlier implementation vs after
+    // assert_eq!(output.forward(&a,&t), );
 }
