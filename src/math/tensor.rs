6ef85c6b170119ffd99da52ca7734440ef591acd
louis <sanicisspeedandfast@gmail.com>
2026-09-09 23:16:23 -0400

    more impl functions

--- src/math/tensor.rs
+++ src/math/tensor.rs
@@ -1,4 +1,5 @@
 use crate::math::rng::Rng;
+use crate::math::matrix::Matrix;
 
 #[derive(Clone, Debug, PartialEq, Default)]
 pub struct Tensor {
@@ -16,5 +17,12 @@ impl Tensor {
     }
     pub fn zeros(n: usize, c: usize, h:usize, w:usize) -> Self { 
         Tensor { vec![0.0 as f32; n*c*h*w], n, c, h, w } 
+    }
+    pub fn n(&self) -> usize { self.n }
+    pub fn c(&self) -> usize { self.c }
+    pub fn h(&self) -> usize { self.h }
+    pub fn w(&self) -> usize { self.w }
+    pub fn to_matrix(&self) -> Matrix {
+        Matrix::new(self.data.clone(), self.n, self.c * self.h * self.w)
     }
 }
