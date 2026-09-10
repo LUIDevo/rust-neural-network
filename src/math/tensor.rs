e5b6d1c2d2a8ad8357094b6be069bea7f9b0f6bb
louis <sanicisspeedandfast@gmail.com>
2026-09-09 23:02:19 -0400

    basic tensor functions

--- src/math/tensor.rs
+++ src/math/tensor.rs
@@ -8,3 +8,13 @@ pub struct Tensor {
     h: usize, // height
     w: usize, // width
 }
+
+impl Tensor {
+    fn new(data: Vec<f32>, n:usize, c: usize, h: usize, w:usize) -> Self {
+        assert_eq!(data.len(), n*c*h*w);
+        Tensor { data, n, c, h, w}    
+    }
+    pub fn zeros(n: usize, c: usize, h:usize, w:usize) -> Self { 
+        Tensor { vec![0.0 as f32; n*c*h*w], n, c, h, w } 
+    }
+}
